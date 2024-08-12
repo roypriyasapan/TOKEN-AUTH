e87fa00db0a819b88f0e927c5ff5dcdfea0e34ce
Priya <Priyasapanroy@gmail.com>
2024-08-12 18:42:50 +0530

    first commit

---
+++ project/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from .views import *
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+
+router = routers.DefaultRouter()
+router.register(r'school',SchoolViewSet, basename='school')
+router.register(r'Student',StudentViewSet,basename='Student')
+
+urlpatterns = [
+    path('api/',include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls'))
+    path('api-token-auth/', views.obtain_auth_token)
+]
